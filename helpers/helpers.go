cc8c030e312158a254ca1140a92bc0a89db217ad
Bronsun <mateusz.broncel1998@gmail.com>
2021-01-01 19:11:33 +0100

    Second default commit : I forgot to save files :)

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,8 +1,11 @@
+// Global functions that can be used in diffrent sections of the project
 package helpers
 
 import (
 	"golang.org/x/crypto/bcrypt"
 )
+
+// Simple error handler
 
 func HandleErr(err error) {
 	if err != nil {
@@ -10,6 +13,7 @@ func HandleErr(err error) {
 	}
 }
 
+// Function that will allow us to hash a password using depencides from golang.org/x/crypto/bcrypt
 func HashAndSalt(pass []byte) string {
 	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
 	HandleErr(err)
