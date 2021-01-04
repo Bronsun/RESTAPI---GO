3e30f1051c4afcd2b6d1ac6c96c057c1b6ce8360
Bronsun <mateusz.broncel1998@gmail.com>
2021-01-04 01:14:44 +0100

    Database folder: I HAVE TO LINK SO IT IS CREATED ONE MORE TIME XD

--- controller/controller.go
+++ controller/controller.go
@@ -1,16 +1,27 @@
 package controller
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/Bronsun/RESTAPI---GO/models"
 	//. "github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type ResponseResult struct {
-	Error  string `json:"error"`
-	Result string `json:"result"`
+	Error string `json:"error"`
 }
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		resp := ResponseResult{Error: "Nothing Posted"}
+		json.NewEncoder(w).Encode(resp)
+	}
+	var formattedBody models.User
+	err = json.Unmarshal(body, &formattedBody)
+	db := database.connectDB()
 
 }
 
---
+++ database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+
+	"gopkg.in/inconshreveable/log15.v2"
+)
+
+func connectDB() *sql.DB {
+	var log = log15.New()
+	db, err := sql.Open("postgres", "dbname=restapi host=localhost user=bronson password=test1234 sslmode = disable")
+	err = db.Ping()
+	if err != nil {
+		log.Error("failed to open database", "err", err)
+	}
+	log.Info("Connected to db")
+	return db
+
+}
